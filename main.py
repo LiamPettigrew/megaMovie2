c9c512945f0243281bb4051ac2e842ce2204fbe7
LiamPettigrew <80730203+LiamPettigrew@users.noreply.github.com>
2022-06-01 23:35:48 +0000

    implemented ticket loop

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 def not_blank(question):
-  valid = False
+    valid = False
 
-  while not valid:
-    response = input(question)
+    while not valid:
+        response = input(question)
 
-    if response != "":
-      return response
+        if response != "":
+            return response
 
-    else:
-      print("Sorry - this can't be blank. "
-           "Please enter your name")
+        else:
+            print("Sorry - this can't be blank. " "Please enter your name")
+
 
 name = not_blank("Name: ")
