99994ccf2b9e4ebff1fec3a375bdcdd6570d020a
LiamPettigrew <80730203+LiamPettigrew@users.noreply.github.com>
2022-06-01 23:17:41 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def not_blank(question):
+  valid = False
+
+  while not valid:
+    response = input(question)
+
+    if response != "":
+      return response
+
+    else:
+      print("Sorry - this can't be blank. "
+           "Please enter your name")
+
+name = not_blank("Name: ")
