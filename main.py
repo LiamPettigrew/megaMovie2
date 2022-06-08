baf3a7bc046b954d0c8ab475b72e94bd351dd984
LiamPettigrew <80730203+LiamPettigrew@users.noreply.github.com>
2022-06-08 22:42:11 +0000

    put all files into one with comments

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+# import statements
+
+# functions go here
 def not_blank(question):
     valid = False
 
@@ -10,5 +13,45 @@ def not_blank(question):
         else:
             print("Sorry - this can't be blank. " "Please enter your name")
 
+# ***** Main routine *****
 
-name = not_blank("Name: ")
+# Set up dictionaries / lists needed to hold data
+
+# Ask user if they used the programme before and show instructions if necessary
+
+# Loop to get ticket details
+
+  # Get name (can't be blank)
+
+  # Get age (between 12 and 130)
+
+name = ""
+count = 0
+MAX_TICKETS = 5
+
+while name != "xxx" and count <= MAX_TICKETS:
+  print("You have {} seats "
+        "left".format(MAX_TICKETS - count))
+
+  name = not_blank("Name: ")
+  count += 1
+  print()
+
+if count == MAX_TICKETS:
+  print("You have sold all the available tickets!")
+else:
+  print("You have sold {} tickets. \n"
+       "There are {} places still available"
+       .format(count, MAX_TICKETS - count))
+
+  # Calculate ticket price
+
+  # Loop to ask for snacks
+
+  # Calculate snack price
+
+# Ask for payment method (and apply surcharge if necessary)
+
+# Calculate total sales and profit
+
+# Output data to text file
